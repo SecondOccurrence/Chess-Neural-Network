5d76ae76536d1d0a46bb43bfb1564cb5438acbf6
SecondOccurrence <jsmith973@protonmail.com>
2024-10-14 20:36:50 +0800

    Finalise data generator

--- src/main.py
+++ src/main.py
@@ -1,6 +1,18 @@
+import numpy as np
+
 from data.chess_data_generator import ChessDataGenerator
 
 data_generator = ChessDataGenerator()
 
-data_generator.generate();
-data_generator.save_dataset(filename="../data/chess_dataset.npz")
+print("Do you fish to load the dataset?")
+
+load_data = input()
+if load_data == 'y':
+  data_generator.load_dataset(filename="../data/chess_dataset.npz")
+else:
+  data_generator.generate();
+  data_generator.save_dataset(filename="../data/chess_dataset.npz")
+
+#print(f"Move index: {test_index}")
+#test_move = list(data_generator.all_possible_moves)[test_index]
+#print(f"Actual move: {test_move}")
